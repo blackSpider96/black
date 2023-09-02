3ea7d9b66113e1208017469c2d4e12d5374e7cee
josuegerardo96 <josuegerardo09@gmail.com>
2023-09-02 11:42:59 -0600

    Se hace rama para los poderes de spiderman

---
+++ poderes.py
@@ -0,0 +1,17 @@
+# Lista de los poderes de Spider-Man
+poderes_spiderman = [
+    "Agilidad sobrehumana",
+    "Fuerza sobrehumana",
+    "Sentido arácnido",
+    "Lanzar telarañas",
+    "Paredes trepadoras",
+]
+
+# Función para imprimir la lista de poderes de Spider-Man
+def imprimir_poderes_spiderman():
+    print("Poderes de Spider-Man:")
+    for poder in poderes_spiderman:
+        print("- " + poder)
+
+# Ejecutar la función para imprimir la lista de poderes
+imprimir_poderes_spiderman()
